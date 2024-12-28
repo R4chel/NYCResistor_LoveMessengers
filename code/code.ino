f549676bb323d694fef191a85a03a47b87c9b1ff
Rachel <rae84@cornell.edu>
2024-12-28 14:54:14 -0500

    print time method 1

--- code/code.ino
+++ code/code.ino
@@ -4,9 +4,12 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 #include "arduino_secrets.h"
+#include "time.h"
 
-
-
+// time settings
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = -5*60*60; // EST 
+const int   daylightOffset_sec = 3600;
 
 
 // LED & button connections to the ESP
@@ -33,6 +36,8 @@ void setup() {
 
   connectWiFi();
   connectFirebase();
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
   digitalWrite(buttonLedPin, HIGH);
 }
 
@@ -44,6 +49,8 @@ void loop() {
     uploadData(buttonState);
     downloadData();
     manageLED(buttonState, firebaseData); // turning the LED on or off
+
+    printLocalTime();
   }
   delay(5);
 }
@@ -103,4 +110,38 @@ void manageLED(int buttonState, int firebaseData) {
   } else {
     digitalWrite(ledPin, LOW);
   }
+}
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
 }
